c5c04bd5b496374c7cb3870c61784fdce41858a9
Sewalam <wahhab.salah.20@gmail.com>
2025-07-12 17:42:00 +0300

    add ProducersService

--- typing_porject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace typing_porject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
