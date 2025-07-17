68cdfbde1a2cbae1eb6a966f509cd0a642cca60c
Sewalam <wahhab.salah.20@gmail.com>
2025-07-18 00:48:31 +0300

    add create cinema functionallity

--- E-tickets/Controllers/CinemasController.cs
+++ E-tickets/Controllers/CinemasController.cs
@@ -1,4 +1,6 @@
 ﻿using E_tickets.Data;
+using E_tickets.Data.Services;
+using E_tickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,17 +8,31 @@ namespace E_tickets.Controllers
 {
     public class CinemasController : Controller
     {
-        private readonly AppDbContext _context;
+        private readonly ICinemasService _service;
 
-        public CinemasController(AppDbContext context)
+        public CinemasController(ICinemasService service)
         {
-            _context = context;
+            _service = service;
         }
 
         public async Task<IActionResult> Index()
         {
-            var allCinemas = await _context.Cinemas.ToListAsync();
+            var allCinemas = await _service.GetallAsync();
             return View(allCinemas);
+        }
+
+        //Get: Cinemas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Logo,Name,Description")] Cinema cinema)
+        {
+            if (!ModelState.IsValid) return View(cinema);
+            await _service.AddAsync(cinema);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ E-tickets/Data/Services/CinemasService.cs
@@ -0,0 +1,14 @@
+﻿using E_tickets.Data.Base;
+using E_tickets.Models;
+
+namespace E_tickets.Data.Services
+{
+   
+        public class CinemasService : EntityBaseRepository<Cinema>, ICinemasService
+        {
+            public CinemasService(AppDbContext context) : base(context)
+            {
+            }
+        }
+    
+}
