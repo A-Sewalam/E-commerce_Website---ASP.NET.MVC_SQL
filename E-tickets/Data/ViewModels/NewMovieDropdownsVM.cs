0ab219086719c4bb5e868098e459c11907fe08a4
Sewalam <wahhab.salah.20@gmail.com>
2025-07-23 00:25:13 +0300

    add partial view for add new botton

---
+++ E-tickets/Data/ViewModels/NewMovieDropdownsVM.cs
@@ -0,0 +1,20 @@
+﻿using E_tickets.Models;
+
+namespace E_tickets.Data.ViewModels
+{
+    public class NewMovieDropdownsVM
+    {
+
+        public NewMovieDropdownsVM()
+        {
+            Producers = new List<Producer>();
+            Cinemas = new List<Cinema>();
+            Actors = new List<Actor>();
+        }
+        public List<Producer> Producers { get; set; }
+        public List<Cinema> Cinemas { get; set; }
+        public List<Actor> Actors { get; set; }
+
+        // and MoveiCategories are Enum
+    }
+}
