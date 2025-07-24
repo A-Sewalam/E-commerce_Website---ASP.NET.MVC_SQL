de408e066bc58e3e79c5334287b228925c2bf94c
Sewalam <wahhab.salah.20@gmail.com>
2025-07-24 11:04:27 +0300

    Add for ShoppingCart class ( AddItemToCart )

--- E-tickets/Data/Cart/ShoppingCart.cs
+++ E-tickets/Data/Cart/ShoppingCart.cs
@@ -1,6 +1,59 @@
-﻿namespace E_tickets.Data.Cart
+﻿using E_tickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_tickets.Data.Cart
 {
     public class ShoppingCart
     {
+
+        public AppDbContext _context { get; set; }
+
+        public string ShoppingCartId { get; set; } // why they are here
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public ShoppingCart(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems
+                .Where(n => n.ShoppingCartId == ShoppingCartId)
+                .Include(n => n.Movie)
+                .ToList());
+        }
+
+
+
+        public double GetShoppingCartTotal() => _context.ShoppingCartItems
+        .Where(n => n.ShoppingCartId == ShoppingCartId)
+        .Select(n => n.Movie.Price * n.Amount)
+        .Sum();
+
+
+        public void AddItemToCart(Movie movie)
+        {
+            var shoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(n => n.Movie.Id == movie.Id && n.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem == null)
+            {
+                shoppingCartItem = new ShoppingCartItem()
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Movie = movie,
+                    Amount = 1
+                };
+
+                _context.ShoppingCartItems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Amount++;
+            }
+            _context.SaveChanges();
+        }
+
+
     }
 }
