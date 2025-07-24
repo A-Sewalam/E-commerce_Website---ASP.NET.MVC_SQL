124fc677dd1bacc9d90e0946b9cdd4dcf383bf4f
Sewalam <wahhab.salah.20@gmail.com>
2025-07-24 10:46:52 +0300

    Add ShoppingCartItem model + add shopping cart class ( dbset , properties , GetShoppingCartItems , GetShoppingCartTotal

--- E-tickets/Data/AppDbContext.cs
+++ E-tickets/Data/AppDbContext.cs
@@ -31,17 +31,18 @@ namespace E_tickets.Data
             base.OnModelCreating(modelBuilder);
         }
 
-            public DbSet<Actor> Actors { get; set; }
-            public DbSet<Movie> Movies { get; set; }
-            public DbSet<Actor_Movie> Actors_Movies { get; set; }
-            public DbSet<Cinema> Cinemas { get; set; }
-            public DbSet<Producer> Producers { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor_Movie> Actors_Movies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
 
 
-             //Orders related tables
-             public DbSet<Order> Orders { get; set; }
-             public DbSet<OrderItem> OrderItems { get; set; }
+        //Orders related tables
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
 
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
     }
-        
+
 }
