bf2bffb4b6d87534491e564be48cdff7a1b350ff
Sewalam <wahhab.salah.20@gmail.com>
2025-08-15 12:18:41 +0300

    add constructor dependincy injection in AccountController

---
+++ E-tickets/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_tickets.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
